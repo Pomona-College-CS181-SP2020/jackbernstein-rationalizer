e6c0a168da6e2c7e67f02ab6f2ddb6f3ba020d92
jackbernstein <john.bernstein@pomona.edu>
2020-04-20 14:47:16 -0500

    fixing build, will it ever work?

--- tests/Example.elm
+++ tests/Example.elm
@@ -2,9 +2,45 @@ module Example exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Html exposing (div, li, ul)
+import Html.Attributes exposing (class)
+import Main
 import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (classes, tag, text)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    describe "The Main module"
+        [ describe "Main.model"
+            [ describe "Main.model.initial"
+                [ test "Initial list has one item" <|
+                    \_ ->
+                        Expect.equal (List.length Main.init.ingredients) 1
+                , test "Inital complete boolean is false" <|
+                    \_ ->
+                        Expect.equal Main.init.complete False
+                ]
+            ]
+        , describe "Main.view"
+            [ describe "Main.view.inital"
+                [ test "Add ingredients button exists" <|
+                    \_ ->
+                        Main.view Main.init
+                            |> Query.fromHtml
+                            |> Query.has [ tag "button", text "Add another ingredient" ]
+                , test "Remove ingredients button exists" <|
+                    \_ ->
+                        Main.view Main.init
+                            |> Query.fromHtml
+                            |> Query.has [ tag "button", text "Remove ingredient" ]
+                , test "Three input fields" <|
+                    \_ ->
+                        Main.view Main.init
+                            |> Query.fromHtml
+                            |> Query.findAll [ tag "input" ]
+                            |> Query.count (Expect.equal 3)
+                ]
+            ]
+        ]
